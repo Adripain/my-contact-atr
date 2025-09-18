541b0cf867cc20c62a8d95dbf673f8a5dce68632
Adripain <adriendtz.pro@gmail.com>
2025-09-18 12:57:05 +0200

    (add) mgdb atlas integration

--- server/index.js
+++ server/index.js
@@ -1,12 +1,22 @@
+require('dotenv').config();
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
 
-const PORT = 3000;
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MGDB Atlas'))
+  .catch(err => console.error('error with mangond ', err));
+
 
 app.get('/', (req, res) => {
   res.json({ message: 'TEst' });
 });
 
+
 app.listen(PORT, () => {
-  console.log(` Serveur sur http://localhost:${PORT}`);
+  console.log(`🚀 Serveur lancé sur http://localhost:${PORT}`);
 });
